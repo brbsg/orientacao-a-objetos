d72c5732c7ad4dbdc432456afd6252c2afda8c7c
barbosaoli <gustavobarbosa7162@gmail.com>
2022-03-08 21:14:10 -0300

    updates

--- LojinhaDeRoupas/src/products/Roupa.java
+++ LojinhaDeRoupas/src/products/Roupa.java
@@ -1,5 +1,23 @@
 package products;
 
-public class Roupa {
+import model.Produto;
+
+public class Roupa extends Produto{
+	private String tamanho;
+
+	public Roupa(int idProduto, String nome, String cor, String categoria, String marca, int preco, String tamanho) {
+		super(idProduto, nome, cor, categoria, marca, preco);
+		this.tamanho = tamanho;
+	}
+
+	public String getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(String tamanho) {
+		this.tamanho = tamanho;
+	}
+	
+	
 
 }
